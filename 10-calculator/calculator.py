3cad0e4a7255e7f5dd0a21a5b6ccc25f999a5158
Jean Matos <jeanpierream@hotmail.com>
2022-07-26 17:17:43 -0300

    Simple Calculator

---
+++ 10-calculator/calculator.py
@@ -0,0 +1,47 @@
+from art import logo
+import os
+def calculator():
+    print(logo)
+    a = float(input("What is the first number? "))
+    continuation(a)
+    
+def continuation(a):
+    op = input("Pick an operation (+, -, *, /): ")
+    b = float(input("Whats the second number? "))
+    if op == "/" and b == 0:
+        print("Error! You can't divide by zero! Try another number.")
+        while b == 0:
+            b = int(input("Whats the second number? "))
+    function = operations[op]
+    result = function(a, b)
+    print(f"{a} {op} {b} = {result}")
+    decision = input(f"Type 'y' to continue calcultating with {result}, or type 'n' to start a new calculation, or 'x' to exit: ").lower()
+    if decision == "y":
+        continuation(result)
+    elif decision == "n":
+        os.system("cls")
+        calculator()
+    elif decision == "x":
+        return
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+operations = {
+    "+" : add,
+    "-" : subtract,
+    "*" : multiply,
+    "/" : divide
+}
+
+result = 0
+calculator()
