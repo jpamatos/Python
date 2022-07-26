b4734d24dd076604fa253d5aa76c642a36e54dcd
Jean Matos <jeanpierream@hotmail.com>
2022-07-26 17:36:35 -0300

    version 11.1

--- 11-black_jack/black_jack.py
+++ 11-black_jack/black_jack.py
@@ -4,41 +4,41 @@ import random, os
 
 
 def start():
+    is_game_over = False
     user_cards = []
     computer_cards = []
     for _ in range(2):
         user_cards.append(deal_card())
         computer_cards.append(deal_card())
-    compare(user_cards, computer_cards)
+    is_game_over = compare(user_cards, computer_cards)
 
 
     while True:
-        if is_game_over:
-                break
-        decision = input("Do you want to draw a card?\n(y/n): ").lower()
-        if decision == "y":
-            user_cards.append(deal_card())
-            compare(user_cards, computer_cards)
-        elif decision == "n":
-            decision2 = input("Do you want to start another game?\n(y/n): ").lower()
-            if decision2 == "y":
-                os.system("cls")
-                start()
-            elif decision2 == "n":
-                return
+        if not is_game_over:
+            decision = input("Do you want to draw a card?\n(y/n): ").lower()
+            if decision == "y":
+                user_cards.append(deal_card())
+                is_game_over = compare(user_cards, computer_cards)
+            elif decision == "n":
+                decision2 = input("Do you want to start another game?\n(y/n): ").lower()
+                if decision2 == "y":
+                    os.system("cls")
+                    start()
+                elif decision2 == "n":
+                    return
+        else:
+            return
     
 def compare(user_cards, computer_cards):
-    if calculate_score(user_cards) == 21 or calculate_score(computer_cards) > 21:
-        print("You Win!")
-        is_game_over = True
-        print(is_game_over)
-    elif calculate_score(computer_cards) == 21 or calculate_score(user_cards) > 21 :
-        print("Dealer Wins!")
-        is_game_over = True
-        print(is_game_over)
-
-    
     print(f"Your cards: {user_cards}, sum: {calculate_score(user_cards)}\nDealer cards: {computer_cards}, sum: {calculate_score(computer_cards)}")
+    if ((calculate_score(user_cards) == 21) or (calculate_score(computer_cards) > 21)):
+        print("You Win!")
+        return True
+    elif ((calculate_score(computer_cards) == 21) or (calculate_score(user_cards) > 21)):
+        print("Dealer Wins!")
+        return True
+    else:
+        return False
 
 def calculate_score(hand):
     score = sum(hand)
@@ -53,7 +53,7 @@ def calculate_score(hand):
 def deal_card():
     return random.choice(cards)
 
-is_game_over = False
+
 print(logo)
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 start()
