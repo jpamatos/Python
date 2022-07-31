bcab1667171aca10b565b24239905805ad9a10b1
Jean Matos <jeanpierream@hotmail.com>
2022-07-31 11:31:04 -0300

    Updated folder name

---
+++ 04-rock_paper_scissors/rock_paper_scissors.py
@@ -0,0 +1,61 @@
+# Import random package
+import random
+
+# rock move
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+# paper move
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+# scissors move
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+# Player choose a move
+player = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+print("You chose: ")
+if player == 0:
+    print(rock)
+elif player == 1:
+    print(paper)
+elif player == 2:
+    print(scissors)
+
+# Computer chose a move randomly
+computer = random.randint(0, 2)
+print("Computer chose: ")
+if computer == 0:
+    print(rock)
+elif computer == 1:
+    print(paper)
+elif computer == 2:
+    print(scissors)
+
+# Game result
+if player == computer:
+    print("It's a draw!")
+else:
+    if (player == computer + 1) or (player == 0 and computer == 2):
+        print("You win!")
+    else:
+        print("You lose!")
