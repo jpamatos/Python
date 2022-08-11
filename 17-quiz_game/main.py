7fa395c4f08c36f9b76a53447a442c19e48c25f1
Jean Matos <jeanpierream@hotmail.com>
2022-08-11 12:29:10 -0300

    project 17

---
+++ 17-quiz_game/main.py
@@ -0,0 +1,6 @@
+from question_model import Question
+from data import question_data
+
+question_bank = [Question(q["text"], q["answer"]) for q in question_data]
+
+print(question_bank[0].text, question_bank[0].answer)
