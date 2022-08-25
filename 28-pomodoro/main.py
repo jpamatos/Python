b773cc96f50922a5bf0bc635e13b314c4b91b205
Jean Matos <jeanpierream@hotmail.com>
2022-08-25 01:23:41 -0300

    version 28.1

--- 28-pomodoro/main.py
+++ 28-pomodoro/main.py
@@ -1,4 +1,5 @@
-
+from tkinter import *
+import time
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -14,5 +15,36 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER MECHANISM ---------------------------- #
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------ #
-
+while True:
+    time.sleep(1)
+    count -= 1
 # ---------------------------- UI SETUP ------------------------------- #
+# Create Window
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+# Create Canvas
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white",
+                   font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+# Create Title
+title = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+title.grid(column=1, row=0)
+
+# Buttons
+start = Button(text="Start", highlightthickness=0)
+start.grid(column=0, row=2)
+
+reset = Button(text="Reset", highlightthickness=0)
+reset.grid(column=2, row=2)
+
+# Checkmarks Label
+check_marks = Label(text="✔", fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
+
+window.mainloop()
