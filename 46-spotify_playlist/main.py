9f1db72825a93d0ef14631b850215f2586229d3f
Jean Matos <jeanpierream@hotmail.com>
2022-09-19 17:51:30 -0300

    version 46.1

--- 46-spotify_playlist/main.py
+++ 46-spotify_playlist/main.py
@@ -8,6 +8,7 @@ import pprint
 from dotenv import load_dotenv
 load_dotenv()
 
+# Get timestamp from user
 date_year = input(
     "Choose: search by date or search by year (type date/year): "
 ).lower()
@@ -16,32 +17,32 @@ if date_year == "date":
                  "Type the date in this format YYYY-MM-DD: ")
     URL = f"https://www.billboard.com/charts/hot-100/{date}"
     target_year = int(date.split("-")[0])
-    year = str(f"{target_year - 2}-{target_year + 2}")
+    year = str(f"{target_year - 1}-{target_year + 1}")
 elif date_year == "year":
     date = input("Year must be at least 2006: ")
     URL = f"https://www.billboard.com/charts/year-end/{date}/hot-100-songs/"
-    year = date
-
+    year = str(f"{int(date) - 1}-{int(date) + 1}")
 
 # Variables
-
 SPOTIFY_CLIENT_ID = os.environ.get("SPOTIFY_ID")
 SPOTIFY_CLIENT_SECRET = os.environ.get("SPOTIFY_SECRET")
 REDIRECT = "http://localhost:8888/callback"
 
-
+# Webscrape billboard to get songs names and artists
 response = requests.get(URL)
 web_page = response.text
 soup = BeautifulSoup(web_page, "html.parser")
 if date_year == "date":
     songs = soup.find_all("h3", class_="a-no-trucate")
+    artists = soup.find_all("span", class_="a-no-trucate")
 elif date_year == "year":
     songs = soup.find_all("h3", class_="a-font-primary-bold-s")
+    artists = soup.find_all("span", class_="a-font-primary-s")
 
 title_list = [(song.getText().strip('\t\n')) for song in songs]
+artist_list = [(artist.getText().strip('\t\n')[:3]) for artist in artists]
 
 # Spotify API
-
 spotify_auth = spotipy.oauth2.SpotifyOAuth(
     client_id=SPOTIFY_CLIENT_ID,
     client_secret=SPOTIFY_CLIENT_SECRET,
@@ -50,6 +51,7 @@ spotify_auth = spotipy.oauth2.SpotifyOAuth(
     cache_path="token.txt"
 )
 
+
 sp = spotipy.Spotify(oauth_manager=spotify_auth)
 
 user_name = sp.current_user()["display_name"]
@@ -57,8 +59,12 @@ user_id = sp.current_user()["id"]
 
 song_uris = []
 
-for song in title_list:
-    result = sp.search(q=f"track:{song} year:{year}", type="track")
+# Create a list of songs
+for song, artist in zip(title_list, artist_list):
+    result = sp.search(
+        q=f"track:{song} artist:{artist}",
+        type="track"
+    )
     try:
         uri = result["tracks"]["items"][0]["uri"]
         song_uris.append(uri)
@@ -71,7 +77,6 @@ playlist = sp.user_playlist_create(
     name=f"{date} Billboard 100",
     public=False
 )
-print(playlist)
 
 # Adding songs found into the new playlist
 sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
