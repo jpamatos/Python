87867999c1b495d0a0de9801aa3ebe6f5c9e672e
Jean Matos <jeanpierream@hotmail.com>
2022-10-14 01:34:33 -0300

    version 63.1

--- 63-library/main.py
+++ 63-library/main.py
@@ -1,23 +1,5 @@
-from enum import unique
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-
-# db = sqlite3.connect("books-collection.db")
-# cursor = db.cursor()
-# # cursor.execute(
-# #     """
-# #     CREATE TABLE books (
-# #         id INTEGER PRIMARY KEY,
-# #         title VARCHAR(250) NOT NULL UNIQUE,
-# #         author VARCHAR(250) NOT NULL,
-# #         rating FLOAT NOT NULL
-# #     )"""
-# # )
-# cursor.execute(
-#     """
-#     INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')"""
-# )
-# db.commit()
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///books.db"
