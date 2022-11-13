0e4440e4cc945ba3e3a4000241958f35515c75f2
Jean Matos <jeanpierream@hotmail.com>
2022-11-12 23:48:29 -0300

    project 81

---
+++ 81-morse_code/morse_code_converter.py
@@ -0,0 +1,30 @@
+CODE = {
+    'a': '.-', 'b': '-...', 'c': '-.-.', 'd': '-..', 'e': '.', 'f': '..-.',
+    'g': '--.', 'h': '....', 'i': '..', 'j': '.---', 'k': '-.-', 'l': '.-..',
+    'm': '--', 'n': '-.', 'o': '---', 'p': '.--.', 'q': '--.-', 'r': '.-.',
+    's': '...', 't': '-', 'u': '..-', 'v': '...-', 'w': '.--', 'x': '-..-',
+    'y': '-.--', 'z': '--..', '1': '.----', '2': '..---', '3': '...--',
+    '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..',
+    '9': '----.', '0': '-----', ', ': '--..--', '.': '.-.-.-', '?': '..--..',
+    '/': '-..-.', '-': '-....-', '(': '-.--.', ')': '-.--.-'
+}
+
+
+def converter(message):
+    converted = ""
+    for letter in message:
+        if letter != " ":
+            converted += CODE[letter]
+        else:
+            converted += " "
+    return converted
+
+
+def main():
+    message = input("Type your message to convert: ").lower()
+    converted = converter(message)
+    print(f"your converted message is: \n{converted}")
+
+
+if __name__ == "__main__":
+    main()
